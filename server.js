94e056ed8e0033df0e6fc55ba3983b3bc9eb2458
Ajurgs <ajjurgs5954@gmail.com>
2021-06-12 14:23:09 -0600

    added the html routes and started api routes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const logger  = require("morgan");
 const mongoose = require("mongoose");
@@ -14,4 +15,39 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb",{useNewUrlParser:true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts",{
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+});
+
+// api routes
+app.get("/api/workouts", (req,res)=>{
+    db.Workout.aggregate([{}])
+})
+app.get("/api/workouts/range", (req,res)=>{
+    
+})
+app.post("/api/workouts", ({body},res)=>{
+    db.Workout.create(body).then(data =>{
+        res.json(data);
+    }).catch(err =>{
+        res.json(err);
+    })
+})
+app.put("/api/workouts/:id", (req,res)=>{
+
+})
+
+// html routes
+app.get("/stats", (req,res) =>{
+    res.sendFile(path.join(__dirname,"./public/stats.html"));
+})
+app.get("/exercise", (req,res) =>{
+    res.sendFile(path.join(__dirname,"./public/exercise.html"));
+})
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
